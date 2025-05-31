903b45ec662e84bf7e50214d5c3042d17b56e0f0
gilangcahyono <gilcah2207@gmail.com>
2025-06-01 02:18:19 +0700

    seting redirect ke github pages

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,8 @@
 import Main from "@/components/Main";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  return redirect("https://gilangcahyono.github.io");
   return (
     <Main>
       <h1 className="text-3xl font-bold text-center">Gilang Cahyono</h1>
